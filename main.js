311deb10aabc7916464633d351e3ef0db331e4b9
Rikas Kabeer <rikascool36@gmail.com>
2023-05-12 10:31:28 +0530

    Work is completed for submit

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-// var typed=new Typed(".input",{
-//     strings:["Flutter Developer","UI/UX Designer","Web App"],
-//     typedSpeed:70,
-//     backSpeed:55,
-//     loop:true
-// })
+
 const form = document.getElementById('form');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
